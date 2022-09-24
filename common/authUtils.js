c9437614210916f8be995048df40b84713719b16
devwillie <swillie100@gmail.com>
2022-09-24 13:12:14 -0700

    Work about half-way through the mods to the 'authUtils.js' file

--- common/authUtils.js
+++ common/authUtils.js
@@ -1,4 +1,33 @@
 import { getClientDetails } from '../services/ClientService';
+import uuid from 'uuid';
+
+const newSessionRoutes = [{ path: '/user/login', method: 'POST'}];
+const authRoutes = [{ path: '/user/password', method: 'PUT'}];
+
+export const isNewSessionRequired = (httpMethod, url) => {
+    for (let routeObj of newSessionRoutes) {
+        if (routeObj.method === httpMethod && routeObj.path === url){
+            return true;
+        }
+
+    }
+
+    return false;
+}
+
+export const isAuthRequired = (httpMethod, url) => {
+    for (let routeObj of authRoutes){
+        if (routeObj.method === httpMethod && routeObj.path === url) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export const generateRandomSessionID = () => {
+    return uuid.v4();
+}
 
 export const clientApiKeyValidation = async (req, res, next) => {
 
